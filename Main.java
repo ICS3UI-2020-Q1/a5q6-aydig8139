60a0d6a4c4871f7ad8bf7d1aa821f9ebff6b82b7
aydig8139 <aydig8139@wrdsb.ca>
2020-10-06 16:20:46 +0000

    finished question to print *s

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * prints *s
+ * @author Rose
  */
 public class Main {
 
@@ -9,7 +11,23 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create new scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //ask user for a number and declare a variable to store it 
+    System.out.println("Please enter a number between 1 and 10");
+    int userNum = input.nextInt();
+
+    //create a for loop and declare a variable to count the number of times the *s are printed on different lines
+    for(int count = 1; count <= userNum; count++){
+
+      //create a nested for loop and declare  variablee to count the number of *s on a single line
+      for(int count2 = 2; count2 <= userNum; count2++){
+       System.out.print("*");
+     }
+      System.out.println("*");
+      
+    }
     
   }
 }
